a4fa8ae86c084947f10f2ad33fbc9c18f26772ad
babama1001980 <102145363+babama1001980@users.noreply.github.com>
2023-08-25 21:36:13 +0800

    Update press_enter.sh

--- press_enter.sh
+++ press_enter.sh
@@ -1,12 +1,13 @@
 ﻿#!/bin/bash
 
+URL="https://raw.githubusercontent.com/babama1001980/giraffe/main/press_enter.sh"
+
 while true; do
-    # 模拟按下 Enter 键
-    xdotool key Return
+    # 使用curl下载文件并保存为press_enter.sh
+    curl -o press_enter.sh "$URL"
     
     # 暂停1分钟
     sleep 60
 done
-
-chmod +x press_enter.sh
-./press_enter.sh
+chmod +x download_script.sh
+./download_script.sh
