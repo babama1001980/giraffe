a399d1e27c0809436bc330fffabae09df5da420a
babama1001980 <102145363+babama1001980@users.noreply.github.com>
2023-08-25 20:10:04 +0800

    Add files via upload

---
+++ automate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+    # 模拟按下数字1
+    xdotool type "1"
+    
+    # 暂停2秒
+    sleep 2
+    
+    # 模拟按下回车键
+    xdotool key Return
+    
+    # 暂停1分钟
+    sleep 60
+done
